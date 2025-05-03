e0ede0b1b2f5cdfbe045887ed81fe7c7f40050a2
Fitra Rahman <39044004+Rizki36@users.noreply.github.com>
2025-05-04 00:01:13 +0700

    fix: create animal

--- src/modules/animal/route.ts
+++ src/modules/animal/route.ts
@@ -1,4 +1,3 @@
-import Elysia, { t } from "elysia";
 import {
 	createAnimalBody,
 	deleteAnimalBody,
@@ -6,106 +5,126 @@ import {
 	updateAnimalBody,
 } from "./model";
 import { type Prisma, PrismaClient } from "../../../prisma/generated/client";
-import { isUndefined } from "../../utils";
+import { isUndefined } from "../common/utils";
+import { elysiaV1Middleware } from "../core/lib/elysia";
 
 const db = new PrismaClient();
 
-export const animalRoute = new Elysia({ prefix: "/animal" })
-	.patch(
-		"",
-		async ({ body }) => {
-			const animal = await db.animal.update({
-				data: {
-					...(!isUndefined(body.code) && { code: body.code }),
-					...(!isUndefined(body.name) && { name: body.name }),
-					...(!isUndefined(body.diedAt) && { diedAt: body.diedAt }),
-					...(!isUndefined(body.dateOfBirth) && {
-						dateOfBirth: body.dateOfBirth,
-					}),
-					...(!isUndefined(body.animalTypeCode) && {
-						animalTypeCode: body.animalTypeCode,
-					}),
-					...(!isUndefined(body.motherId) && {
-						motherId: body.motherId,
-					}),
-					...(!isUndefined(body.fatherId) && {
-						fatherId: body.fatherId,
-					}),
-					...(!isUndefined(body.note) && {
-						note: body.note,
-					}),
-				},
-				where: {
-					id: body.id,
-				},
-			});
+export const animalRoute = elysiaV1Middleware.group("/animal", (app) => {
+	return app
+		.patch(
+			"",
+			async ({ body }) => {
+				const animal = await db.animal.update({
+					data: {
+						...(!isUndefined(body.code) && { code: body.code }),
+						...(!isUndefined(body.name) && { name: body.name }),
+						...(!isUndefined(body.diedAt) && { diedAt: body.diedAt }),
+						...(!isUndefined(body.dateOfBirth) && {
+							dateOfBirth: body.dateOfBirth,
+						}),
+						...(!isUndefined(body.animalTypeCode) && {
+							animalTypeCode: body.animalTypeCode,
+						}),
+						...(!isUndefined(body.motherId) && {
+							motherId: body.motherId,
+						}),
+						...(!isUndefined(body.fatherId) && {
+							fatherId: body.fatherId,
+						}),
+						...(!isUndefined(body.note) && {
+							note: body.note,
+						}),
+					},
+					where: {
+						id: body.id,
+					},
+				});
 
-			return {
-				doc: animal,
-			};
-		},
-		{
-			body: updateAnimalBody,
-		},
-	)
-	.post(
-		"",
-		async ({ body }) => {
-			const animal = await db.animal.create({
-				data: body,
-			});
+				return {
+					doc: animal,
+				};
+			},
+			{
+				body: updateAnimalBody,
+			},
+		)
+		.post(
+			"",
+			async ({ body, jwt, cookie }) => {
+				// Verify token from cookie
+				const payload = await jwt.verify(cookie.authToken.value);
+				if (!payload) return { authenticated: false };
 
-			return {
-				doc: animal,
-			};
-		},
-		{
-			body: createAnimalBody,
-		},
-	)
-	.delete(
-		"",
-		async ({ body }) => {
-			const animal = await db.animal.delete({
-				where: {
-					id: body.id,
-				},
-			});
+				const animal = await db.animal.create({
+					data: {
+						code: body.code,
+						name: body.name,
+						animalType: {
+							connect: {
+								code: body.animalTypeCode,
+							},
+						},
+						user: {
+							connect: {
+								id: payload.id,
+							},
+						},
+					},
+				});
 
-			return {
-				doc: animal,
-			};
-		},
-		{
-			body: deleteAnimalBody,
-		},
-	)
-	.get(
-		"/list",
-		async ({ query }) => {
-			const limit = query.limit ?? 20;
+				return {
+					doc: animal,
+				};
+			},
+			{
+				body: createAnimalBody,
+			},
+		)
+		.delete(
+			"",
+			async ({ body }) => {
+				const animal = await db.animal.delete({
+					where: {
+						id: body.id,
+					},
+				});
 
-			const where: Prisma.AnimalFindManyArgs["where"] = {};
-			if (query.search)
-				where.OR = [
-					{ code: { contains: query.search } },
-					{ name: { contains: query.search } },
-				];
-			if (query.id_eq) where.id = query.id_eq;
-			if (query.id_ne) where.id = { not: query.id_ne };
-			if (query.animal_type_code_eq)
-				where.animalTypeCode = query.animal_type_code_eq;
-			if (query.gender_eq === "MALE") where.gender = { equals: "MALE" };
-			if (query.gender_eq === "FEMALE") where.gender = { equals: "FEMALE" };
+				return {
+					doc: animal,
+				};
+			},
+			{
+				body: deleteAnimalBody,
+			},
+		)
+		.get(
+			"/list",
+			async ({ query }) => {
+				const limit = query.limit ?? 20;
 
-			const animals = await db.animal.findMany({ where, take: limit });
+				const where: Prisma.AnimalFindManyArgs["where"] = {};
+				if (query.search)
+					where.OR = [
+						{ code: { contains: query.search } },
+						{ name: { contains: query.search } },
+					];
+				if (query.id_eq) where.id = query.id_eq;
+				if (query.id_ne) where.id = { not: query.id_ne };
+				if (query.animal_type_code_eq)
+					where.animalTypeCode = query.animal_type_code_eq;
+				if (query.gender_eq === "MALE") where.gender = { equals: "MALE" };
+				if (query.gender_eq === "FEMALE") where.gender = { equals: "FEMALE" };
 
-			return {
-				docs: animals,
-				limit,
-			};
-		},
-		{
-			query: listAnimalQuery,
-		},
-	);
+				const animals = await db.animal.findMany({ where, take: limit });
+
+				return {
+					docs: animals,
+					limit,
+				};
+			},
+			{
+				query: listAnimalQuery,
+			},
+		);
+});
---
+++ src/modules/core/lib/elysia.ts
@@ -0,0 +1,30 @@
+import cookie from "@elysiajs/cookie";
+import jwt from "@elysiajs/jwt";
+import Elysia, { t } from "elysia";
+import { oauth2 } from "elysia-oauth2";
+
+export const elysia = new Elysia();
+
+const jwtBodySchema = t.Object({
+	id: t.String(),
+	email: t.String(),
+	exp: t.Number(),
+});
+
+export const elysiaV1Middleware = elysia
+	.use(cookie())
+	.use(
+		jwt<"jwt", typeof jwtBodySchema>({
+			name: "jwt",
+			secret: process.env.JWT_SECRET || "your-secret-key",
+		}),
+	)
+	.use(
+		oauth2({
+			Google: [
+				process.env.GOOGLE_CLIENT_ID!,
+				process.env.GOOGLE_CLIENT_SECRET!,
+				process.env.GOOGLE_REDIRECT_URI!,
+			],
+		}),
+	);
